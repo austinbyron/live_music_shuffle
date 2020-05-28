4bf0eed241490be0abd5d74a89cff8136168d27f
Austin Byron <austin.t.byron@gmail.com>
2020-05-28 10:56:15 -0400

    changed git ignore and small comments

--- lib/songs_update_backroundplay.dart
+++ lib/songs_update_backroundplay.dart
@@ -57,27 +57,27 @@ int countMusicPlayer = 0;
 var tempInt;
 
 MediaControl playControl = MediaControl(
-  androidIcon: 'drawable/ic_action_play_arrow',
+  //androidIcon: 'drawable/ic_action_play_arrow',
   label: 'Play',
   action: MediaAction.play,
 );
 MediaControl pauseControl = MediaControl(
-  androidIcon: 'drawable/ic_action_pause',
+  //androidIcon: 'drawable/ic_action_pause',
   label: 'Pause',
   action: MediaAction.pause,
 );
 MediaControl skipToNextControl = MediaControl(
-  androidIcon: 'drawable/ic_action_skip_next',
+  //androidIcon: 'drawable/ic_action_skip_next',
   label: 'Next',
   action: MediaAction.skipToNext,
 );
 MediaControl skipToPreviousControl = MediaControl(
-  androidIcon: 'drawable/ic_action_skip_previous',
+  //androidIcon: 'drawable/ic_action_skip_previous',
   label: 'Previous',
   action: MediaAction.skipToPrevious,
 );
 MediaControl stopControl = MediaControl(
-  androidIcon: 'drawable/ic_action_stop',
+  //androidIcon: 'drawable/ic_action_stop',
   label: 'Stop',
   action: MediaAction.stop,
 );
