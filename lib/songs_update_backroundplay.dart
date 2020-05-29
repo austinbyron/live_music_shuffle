94a7f45b07f24fb01036814565a10b5e4ace67f7
Austin Byron <austin.t.byron@gmail.com>
2020-05-29 01:23:54 -0400

    didn't like the adaptive sliders:

--- lib/songs_update_backroundplay.dart
+++ lib/songs_update_backroundplay.dart
@@ -300,7 +300,7 @@ class MusicPlayerState extends State<MusicPlayer> {
         Text("\nVolume"),
         StreamBuilder<double>(
           stream: _volumeSubject.stream,
-          builder: (context, snapshot) => Slider.adaptive(
+          builder: (context, snapshot) => Slider(
             divisions: 20,
             min: 0.0,
             max: 2.0,
@@ -314,7 +314,7 @@ class MusicPlayerState extends State<MusicPlayer> {
         Text("Speed"),
         StreamBuilder<double>(
           stream: _speedSubject.stream,
-          builder: (context, snapshot) => Slider.adaptive(
+          builder: (context, snapshot) => Slider(
             divisions: 10,
             min: 0.5,
             max: 1.5,
@@ -434,7 +434,7 @@ class SeekBarState extends State<SeekBar> {
           
         ),
         //Text("Track position"),
-        Slider.adaptive(
+        Slider(
           min: 0.0,
           max: widget.duration.inMilliseconds.toDouble() > 0.0 ? widget.duration.inMilliseconds.toDouble() : 0.0,
           value: widget.position.inMilliseconds.toDouble() > (_dragValue ?? 0.0) ? (_dragValue ?? 0.0): 0.0,
